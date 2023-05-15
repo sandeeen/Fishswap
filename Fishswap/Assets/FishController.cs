1cf408afcf257cdde0b576a582dfc8aadb378660
sandeeen <60133440+sandeeen@users.noreply.github.com>
2023-05-15 15:49:38 +0200

    :fish:

--- Fishswap/Assets/FishController.cs
+++ Fishswap/Assets/FishController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class FishController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5f;
+
 
     private Camera mainCamera;
+    private Vector3 targetPos;
 
+    float mouseX;
+    float mouseY;
 
     private void Awake()
     {
-       
+
     }
 
     void Start()
@@ -20,19 +25,43 @@ public class FishController : MonoBehaviour
 
     void Update()
     {
-        RaycastOnMousePos();
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        if (mouseX != 0f || mouseY != 0f)
+        {
+            RaycastOnMousePos();
+
+        }
 
         UpdateFishPos();
+
 
     }
 
     private void RaycastOnMousePos()
     {
+
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPos = hit.point;
+
+        }
+        else
+        {
+            Debug.Log("Raycast did not hit");
+        }
     }
 
     private void UpdateFishPos()
     {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        transform.LookAt(targetPos);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 90);
     }
 
 }
